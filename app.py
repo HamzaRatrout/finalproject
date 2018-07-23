53843da5bdb2b92d41d5c16814030b7c774b2ad6
HamzaRatrout <hamzaratrout60@gmail.com>
2018-07-23 12:59:53 +0300

    changes

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import dataset, random, os
 
 app = Flask(__name__)
-app.secret_key = urandom(24)
+# app.secret_key = urandom(24)
 
-TODO: set up database
-# db = dataset.connect(“<database>”)
+
 
 
 @app.route('/')
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
 @app.route('/home')
 def homepage():
 	return render_template('home.html')
