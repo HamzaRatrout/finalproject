96610d1944b5f44c4a4ae1dcfb2af534c24b6046
mohammadbarbarawi <mohammad.barbarawi2016@gmail.com>
2018-07-23 16:54:31 +0300

    newmessage

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import dataset, random, os
 
 app = Flask(__name__)
-# app.secret_key = urandom(24)
+db_url=os.environ['DATABASE_URL']
+app.secret_key = urandom(24)
 
 
 
@@ -14,9 +15,23 @@ def login_page():
 @app.route('/home')
 def homepage():
 	return render_template('home.html')
+@app.route("/<page_name>/")
+def load_generic_page(page_name):
+	return render_template(page_name + "1.html",title=page_name)
 
+@app.route("/signin", methods=['POST', 'GET'])
+def login_page():
+	accounts=db["accounts"]
+	username=request.form['userName']
+	password=request.form['password']
+	user = accounts.find_one(userName=username,password=password)
+	if user:
+		session['loggedIn']=True
+		return render_template("homepage.html")
+	else :
+		return render_template("login1.html",error="The password or username is incorrect")
+		
 # TODO: route to /register
-
 # TODO: route to /error
 
 if __name__ == "__main__":
