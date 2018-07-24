232ec4a66306bb9d53de1f3af6fd7d0661daab35
mohammadbarbarawi <mohammad.barbarawi2016@gmail.com>
2018-07-24 12:32:55 +0300

    newmessage

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def login_page1():
     user = accounts.find_one(userName=username,password=password)
     if user:
         session['loggedIn']=True
-        return redirect("home.html")
+        return redirect("/home")
     else :
         return render_template("login.html",error="The password or username is incorrect")
 
